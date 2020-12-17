11776cd4e8d3c79e2c04b5148c09d89d8b58544e
bin549 <549288160@qq.com>
2020-12-17 20:34:15 +0800

    Spacy chinese test. load zh_sm. token test.

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+from spacy.lang.zh import Chinese
+import spacy
 
 
 def main():
-    pass
+    text = "小龙女的男朋友是周恩来。"
+    nlp = Chinese()
+    nlp.add_pipe(nlp.create_pipe('sentencizer'))
+    document = nlp(text)
+    print(document)
+    sentences = [sent.string.strip() for sent in document.sents]
+    nlp_model = spacy.load('zh_core_web_sm')
+    triples = []
+
+    subject = ''
+    object = ''
+    relation = ''
+    subjectConstruction = ''
+    objectConstruction = ''
+
+    for sentence in sentences:
+        tokens = nlp_model(sentence)
+        for token in tokens:
+            print(token.text, "->", token.dep_)
+
+
+        # triples.append(processSentence(sentence))
+
+
+
+
 
 
 if __name__ == '__main__':
