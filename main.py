ae866dc2514762c9bb1b78d842f76abaa26b2d55
bin549 <549288160@qq.com>
2020-12-22 23:24:24 +0800

    Basic English Triple Extraction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from spacy.lang.en import English
 import spacy
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def isRelationCandidate(token):
@@ -16,58 +18,78 @@ def appendChunk(original, chunk):
     return original + ' ' + chunk
 
 
-def main():
-    text = "London is the city of England."
-    nlp = English()
-    nlp.add_pipe(nlp.create_pipe('sentencizer'))
-    document = nlp(text)
-    print(document)
-    sentences = [sent.string.strip() for sent in document.sents]
-    nlp_model = spacy.load('en_core_web_sm')
-    triples = []
-
+def processSubjectObjectPairs(tokens):
     subject = ''
     object = ''
     relation = ''
-    subjectConstruction = ''
-    objectConstruction = ''
-
-    for sentence in sentences:
-        tokens = nlp_model(sentence)
-        for token in tokens:
-            print(token.text, "->", token.dep_)
-            if "punct" in token.dep_:
-                continue
-            if isRelationCandidate(token):
-                relation = appendChunk(relation, token.lemma_)
-            if isConstructionCandidate(token):
-                if subjectConstruction:
-                    subjectConstruction = appendChunk(
-                        subjectConstruction, token.text)
-                if objectConstruction:
-                    objectConstruction = appendChunk(
-                        objectConstruction, token.text)
-            if "subj" in token.dep_:
-                subject = appendChunk(subject, token.text)
-                subject = appendChunk(subjectConstruction, subject)
-                subjectConstruction = ''
-            if "obj" in token.dep_:
-                object = appendChunk(object, token.text)
-                object = appendChunk(objectConstruction, object)
-                objectConstruction = ''
-            print(subject.strip(), ",", relation.strip(), ",", object.strip())
-            return (subject.strip(), relation.strip(), object.strip())
-
-    print(subjectConstruction)
-    print(objectConstruction)
-            # if isRelationCandidate(token):
-            #     relation = appendChunk(relation, token.lemma_)
-            #     print(relation)
+    for token in tokens:
+        if "punct" in token.dep_:
+            continue
+        if isRelationCandidate(token):
+            relation = appendChunk(relation, token.lemma_)
+        if "subj" in token.dep_:
+            subject = appendChunk(subject, token.text)
+        if "obj" in token.dep_:
+            object = appendChunk(object, token.text)
+    return (subject.strip(), relation.strip(), object.strip())
 
 
-        # triples.append(processSentence(sentence))
+def processSentence(sentence):
+    tokens = nlp_model(sentence)
+    return processSubjectObjectPairs(tokens)
+
+
+def printGraph(triples):
+    G = nx.Graph()
+    for triple in triples:
+        G.add_node(triple[0])
+        G.add_node(triple[1])
+        G.add_node(triple[2])
+        G.add_edge(triple[0], triple[1])
+        G.add_edge(triple[1], triple[2])
+    pos = nx.spring_layout(G)
+    plt.figure()
+    nx.draw(G, pos, edge_color='black', width=1, linewidths=1,
+            node_size=500, node_color='seagreen', alpha=0.9,
+            labels={node: node for node in G.nodes()})
+    plt.axis('off')
+    plt.show()
 
 
 
 if __name__ == '__main__':
-    main()
+    text = "London is the city of England."
+    nlp = English()
+    nlp.add_pipe(nlp.create_pipe('sentencizer'))
+    document = nlp(text)
+    sentences = [sent.string.strip() for sent in document.sents]
+    nlp_model = spacy.load('en_core_web_sm')
+    triples = []
+
+    for sentence in sentences:
+        tokens = nlp_model(sentence)
+        triples.append(processSentence(sentence))
+
+    print(triples)
+    printGraph(triples)
+
+
+    # return (subject.strip(), relation.strip(), object.strip())
+
+            #     if subjectConstruction:
+            #         subjectConstruction = appendChunk(subjectConstruction, token.text)
+            #         print(subjectConstruction)
+            #     if objectConstruction:
+            #         objectConstruction = appendChunk(
+            #             objectConstruction, token.text)
+
+            #     subject = appendChunk(subjectConstruction, subject)
+            #     subjectConstruction = ''
+
+            #     object = appendChunk(objectConstruction, object)
+            #     objectConstruction = ''
+
+    # print(subjectConstruction)
+    # print(objectConstruction)
+
+        # triples.append(processSentence(sentence))
