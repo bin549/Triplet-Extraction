9e317a4f37fe751e0485f2fb726d69386fa19e0d
bin549 <549288160@qq.com>
2021-01-23 19:36:14 +0800

    I have to Finish it.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wikipedia
+import spacy
 
 
 class TextExtractor:
@@ -21,9 +22,31 @@ class TextExtractor:
         return f.read()
 
 
+class TextExtractorPipe:
+
+    __textExtractors: [TextExtractor]
+
+    def __init__(self):
+        self.__textExtractors = []
+
+    def addTextExtractor(self, textExtractor: TextExtractor):
+        self.__textExtractors.append(textExtractor)
+
+    def extract(self) -> str:
+        result = ''
+        for textExtractor in self.__textExtractors:
+            result = result + textExtractor.getText()
+        return result
+
+
 def main():
     textExtractor1 = TextExtractor("WWII", "Q362")
     textExtractor1.extract()
+    textExtractorPipe = TextExtractorPipe()
+    textExtractorPipe.addTextExtractor(textExtractor1)
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe(nlp.create_pipe('sentencizer'), before="parser")
+    doc = nlp(textExtractorPipe.extract())
 
 
 if __name__ == '__main__':
